2b482ed0acf6e4d2800f2c661dcf60f70f5cea65
JosefKru <josefkaru@gmail.com>
2022-11-23 11:20:22 +0300

    checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import Head from 'next/head'
+import Header from '../components/Header'
+import { useSelector } from 'react-redux'
+import { selectBasketItems } from '../redux/basketSlice'
 
 const Checkout = () => {
-  return <div>Checkout</div>
+  const items = useSelector(selectBasketItems)
+  return (
+    <div>
+      <Head>
+        <title>Bag - Room4mommy</title>
+      </Head>
+
+      <Header />
+      <main>
+        <div>
+          <h1>{items.lenght > 0 ? 'Review your bag' : 'Your bag is empt'}</h1>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Checkout
