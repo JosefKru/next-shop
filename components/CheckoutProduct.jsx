97e01e1df150d7394928f5f8305eda7c69cd549e
JosefKru <josefkaru@gmail.com>
2022-11-23 23:25:55 +0300

    added checkout page

---
+++ components/CheckoutProduct.jsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+import Currency from 'react-currency-formatter'
+import { urlFor } from '../lib/client'
+import { GrFormDown } from 'react-icons/gr'
+import { useDispatch } from 'react-redux'
+import { removeFromBasket } from '../redux/basketSlice'
+import toast from 'react-hot-toast'
+
+const CheckoutProduct = ({ items, id }) => {
+  const dispatch = useDispatch()
+
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }))
+
+    toast.error(`${items[0].title} removed from basket`, {
+      position: 'bottom-center',
+    })
+  }
+
+  return (
+    <div className="flex flex-col gap-x-4 border-b border-gray-300 py-5 lg:flex-row lg:items-center">
+      <div className="relative h-44 w-44">
+        <Image
+          src={urlFor(items[0].image).url()}
+          layout="fill"
+          objectFit="contain"
+          alt="product"
+        />
+      </div>
+
+      <div className="flex flex-1 items-end lg:items-center">
+        <div className="flex-1 space-y-4">
+          <div className="flex flex-col gap-x-8 text-xl lg:flex-row lg:text-2xl">
+            <h4 className="font-semibold lg:w-96">{items[0].title}</h4>
+            <p className="flex items-end gap-x-1 font-semibold">
+              {items.length}
+              <GrFormDown className="h-6 w-6" color="blue" />
+            </p>
+          </div>
+
+          <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+            Show product details
+            <GrFormDown className="h-6 w-6" color="#56b0f2" />
+          </p>
+        </div>
+        <div className="flex flex-col items-end space-y-4">
+          <h4 className="text-xl font-semibold lg:text-2xl">
+            <Currency
+              quantity={items.reduce((total, item) => total + item.price, 0)}
+              currency="UAH"
+            />
+          </h4>
+          <button
+            onClick={removeItemFromBasket}
+            className="text-blue-500 hover:underline"
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,21 +1,66 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import CheckoutProduct from '../components/CheckoutProduct'
+import Button from '../components/Button'
 import { useSelector } from 'react-redux'
-import { selectBasketItems } from '../redux/basketSlice'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { selectBasketItems, selectBasketTotal } from '../redux/basketSlice'
+import Currency from 'react-currency-formatter'
 
 const Checkout = () => {
+  const basketTotal = useSelector(selectBasketTotal)
   const items = useSelector(selectBasketItems)
+  const router = useRouter()
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState({})
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      ;(results[item._id] = results[item._id] || []).push(item)
+      return results
+    }, {})
+
+    setGroupedItemsInBasket(groupedItems)
+  }, [items])
+
   return (
-    <div>
+    <div className="min-h-screen overflow-hidden bg-gradient-to-b from-[#f0f7fd]">
       <Head>
         <title>Bag - Room4mommy</title>
       </Head>
 
       <Header />
-      <main>
-        <div>
-          <h1>{items.lenght > 0 ? 'Review your bag' : 'Your bag is empt'}</h1>
+      <main className="mx-auto max-w-5xl pb-24">
+        <div className="px-5 ">
+          <h1 className="my-4 text-3xl font-semibold lg:text-4xl">
+            {items.length > 0 ? 'Review your bag' : 'Your bag is empt'}
+          </h1>
+          <p className="my-4">Bla bla bla bla</p>
+
+          {items.length === 0 && (
+            <Button
+              title="Continue Shopping"
+              onClick={() => router.push('/')}
+            />
+          )}
         </div>
+
+        {items.length > 0 && (
+          <div className="mx-5 md:mx-8">
+            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+              <CheckoutProduct key={key} items={items} id={key} />
+            ))}
+            <div className="my-12 mt-6 ml-auto max-w-3xl">
+              <div className="pb-4 ">
+                <div className="flex justify-between pt-4 text-xl font-semibold">
+                  <p>Total</p>
+                  <p>
+                    <Currency quantity={basketTotal} currency="UAH" />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   )
